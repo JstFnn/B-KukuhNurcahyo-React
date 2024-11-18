a22bdf3590e152438ceea873eb3d02c7c8e1c0f4
Your Name <kukuhnurcahyo64@gmail.com>
2024-11-18 15:54:28 +0700

    first commit

---
+++ src/services/getAllProductCategories.jsx
@@ -0,0 +1,15 @@
+const getAllProductCategories = ()  => {
+  return [   {
+    id: 'MEN',
+    slug: 'men-parfume',
+    name: `Men's Parfume`
+  },
+  {
+    id: 'WOMEN',
+    slug: 'women-parfume',
+    name: `Women's parfume`
+  },
+  ]
+}
+
+export default getAllProductCategories
---
+++ src/services/getAllProducts.js
@@ -0,0 +1,93 @@
+export default function getAllProducts() {
+  return [
+    {
+      id: 'EDT-WOMEN-98-S',
+      slug: 'SOTB',
+      name: "SOTB",
+      category: `Women's parfume`,
+      categorySlug: 'women-parfume',
+      price: 549_000,
+      stock: 100,
+      imageUrl: '/assets/images/SOTB.jpg',
+      description: `TokoBDG merupakan brand inspired parfum berkualitas tinggi yang terinspirasi dari bau parfum brand ternama.`
+    },
+    {
+      id: 'EDP-WOMEN-99-S',
+      slug: 'SCANDALOUS',
+      name: "SCANDALOUS",
+      category: `Women's parfume`,
+      categorySlug: 'women-parfume',
+      price: 920_000,
+      stock: 5,
+      imageUrl: `/assets/images/SCANDALOUS.jpg`,
+      description: `TokoBDG merupakan brand inspired parfum berkualitas tinggi yang terinspirasi dari bau parfum brand ternama.`
+    },
+    {
+      id: 'EDP-WOMEN-99-O',
+      slug: 'ORGASM',
+      name: "ORGASM",
+      category: `Women's parfume`,
+      categorySlug: 'women-parfume',
+      price: 580_000,
+      stock: 0,
+      imageUrl: `/assets/images/ORGASM.jpg`,
+      description: `TokoBDG merupakan brand inspired parfum berkualitas tinggi yang terinspirasi dari bau parfum brand ternama.`
+    },
+    {
+      id: 'EDP-WOMEN-99-P',
+      slug: 'PERFECTION',
+      name: "PERFECTION",
+      category: `Women's parfume`,
+      categorySlug: 'women-parfume',
+      price: 909_000,
+      stock: 5,
+      imageUrl: `/assets/images/PERFECTION.jpg`,
+      description: `TokoBDG merupakan brand inspired parfum berkualitas tinggi yang terinspirasi dari bau parfum brand ternama.`
+    },
+    {
+      id: 'EDT-MEN-99-U',
+      slug: 'UNROSED',
+      name: "UNROSED",
+      category: `Men's Parfume`,
+      categorySlug: 'men-parfume',
+      price: 500_000,
+      stock: 100,
+      imageUrl: '/assets/images/UNROSED.jpg',
+      description: `TokoBDG merupakan brand inspired parfum berkualitas tinggi yang terinspirasi dari bau parfum brand ternama.`
+    },  
+    {
+      id: 'EDT-MEN-98-S',
+      slug: 'SAVAGE',
+      name: "SAVAGE",
+      category: `Men's Parfume`,
+      categorySlug: 'men-parfume',
+      price: 649_000,
+      stock: 100,
+      imageUrl: '/assets/images/SAVAGE.jpg',
+      description: `TokoBDG merupakan brand inspired parfum berkualitas tinggi yang terinspirasi dari bau parfum brand ternama.`
+    },
+    {
+      id: 'EDP-MEN-95-OM',
+      slug: 'ONE MILLION',
+      name: "ONE MILLION",
+      category: `Men's Parfume`,
+      categorySlug: 'men-parfume',
+      price: 389_000,
+      stock: 100,
+      imageUrl: '/assets/images/ONE MILLION.jpg',
+      description: `TokoBDG merupakan brand inspired parfum berkualitas tinggi yang terinspirasi dari bau parfum brand ternama.`
+    },
+    {
+      id: 'EDP-MEN-99-A',
+      slug: 'AVENTUS',
+      name: "AVENTUS",
+      category: `Men's Parfume`,
+      categorySlug: 'men-parfume',
+      price: 479_000,
+      stock: 100,
+      imageUrl: '/assets/images/AVENTUS.jpg',
+      description: `TokoBDG merupakan brand inspired parfum berkualitas tinggi yang terinspirasi dari bau parfum brand ternama.`
+    },
+
+  ]
+}
